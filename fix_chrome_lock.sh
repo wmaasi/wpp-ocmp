b5ccfe4d76a7494ba9e68183e12774a28477ddb5
William Maas <william.maas@4thwave.solutions>
2026-04-06 17:27:15 -0600

    chore: eliminar fix_chrome_lock.sh, cron.js y test.js

--- fix_chrome_lock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-TOKEN_DIR="/home/william_maas/wpp-ocmp/tokens/ocmp-bot"
-
-echo "🔧 Eliminando locks de Chromium..."
-rm -f $TOKEN_DIR/SingletonLock
-rm -f $TOKEN_DIR/SingletonCookie
-rm -f $TOKEN_DIR/SingletonSocket
-
-echo "🔧 Locks limpiados."
